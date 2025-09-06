6d9b14b7c42d5d1e1719917f801fb2f01b4c9ee8
Sarthak Yadav <136995087+Sarthakyadav783@users.noreply.github.com>
2025-09-06 13:49:33 +0530

    Delete add_project_manager_column.sql

--- add_project_manager_column.sql
+++
@@ -1,4 +0,0 @@
--- Add project_manager column to existing projects table
-ALTER TABLE projects ADD COLUMN project_manager INT DEFAULT NULL;
-ALTER TABLE projects ADD FOREIGN KEY (project_manager) REFERENCES users(id) ON DELETE SET NULL;
-ALTER TABLE projects ADD INDEX idx_project_manager (project_manager);
